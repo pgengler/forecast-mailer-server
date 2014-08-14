91f9be40d993dc1386a7cce9028a45f3844d6811
Phil Gengler <phil@pgengler.net>
2014-08-14 15:00:51 -0400

    List all subscriptions in subscriptions#index

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,19 +1,5 @@
 class SubscriptionsController < ApplicationController
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def delete
-  end
+	def index
+		@subscriptions = Subscription.all
+	end
 end
