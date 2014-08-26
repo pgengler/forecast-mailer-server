be3a16576c909b306ef596b8dbc86755daee977d
Phil Gengler <phil@pgengler.net>
2014-08-26 14:53:42 -0400

    Make forecast fetching/mailing happen in a Sidekiq worker instead of a Rake task

---
+++ app/workers/forecast_mailer_worker.rb
@@ -0,0 +1,24 @@
+class ForecastMailerWorker
+	include Sidekiq::Worker
+
+	def perform(subscription_id)
+		subscription = Subscription.find(id)
+		forecast = get_forecast(subscription)
+		ForecastMailer.daily(subscription, forecast).deliver
+	end
+
+	def self.send_forecast_emails
+		Subscription.all.each do |subscription|
+			if subscription.geocoded?
+				perform_async(subscription.id)
+			end
+		end
+	end
+
+	private
+
+	def get_forecast(subscription)
+		ForecastIO.api_key = Rails.application.secrets.forecastio_api_key
+		ForecastIO.forecast(subscription.latitude, subscription.longitude, params: { units: 'si' })
+	end
+end
