ee32b22202f643c97c9b2b80011821f53c4db778
Phil Gengler <phil@pgengler.net>
2014-08-27 15:35:34 -0400

    Add Subscription.active method for returning only active subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,8 @@ class Subscription < ActiveRecord::Base
 
 	geocoded_by :location
 	after_validation ->(obj) { BackgroundGeocoder.perform_async(obj.id) }, if: ->(obj) { obj.location_changed? }
+
+	def self.active
+		where('(start IS NULL AND end IS NULL) OR (start IS NULL AND ? <= end) OR (end IS NULL AND start <= ?) OR (? BETWEEN start AND end)', Date.today, Date.today, Date.today)
+	end
 end
