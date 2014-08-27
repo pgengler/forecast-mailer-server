25fbf7893fd372af8a578a6cd645816da37a88b5
Phil Gengler <phil@pgengler.net>
2014-08-27 14:32:44 -0400

    Update restart step in Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,22 +18,13 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+	desc 'Restart application'
+	task :restart do
+		on roles(:app), in: :sequence, wait: 5 do
+			invoke 'unicorn:stop'
+			invoke 'unicorn:start'
+		end
+	end
 
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+	after :publishing, :restart
 end
