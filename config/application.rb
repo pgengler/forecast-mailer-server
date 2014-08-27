e24dfabd6f1b5919a664a3b0778eb3b36b9ecf10
Phil Gengler <phil@pgengler.net>
2014-08-27 15:57:31 -0400

    Rename app from 'WeatherForecasts' to 'ForecastMailer'

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WeatherForecasts
+module ForecastMailer
 	class Application < Rails::Application
 		config.action_mailer.smtp_settings = {
 			address: 'smtp.mandrillapp.com',
