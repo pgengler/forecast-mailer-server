9ac84ce186579586729daaf1029cc1ac25b5a08b
Phil Gengler <phil@pgengler.net>
2014-08-28 12:57:56 -0400

    Don't start background geocoding until after new records are committed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,9 +3,15 @@ class Subscription < ActiveRecord::Base
 	validates :email, presence: true
 
 	geocoded_by :location
-	after_validation ->(obj) { BackgroundGeocoder.perform_async(obj.id) }, if: ->(obj) { obj.location_changed? }
+	after_commit :geocode_in_background, on: [ :create, :update ]
 
 	def self.active
 		where('(start IS NULL AND end IS NULL) OR (start IS NULL AND ? <= end) OR (end IS NULL AND start <= ?) OR (? BETWEEN start AND end)', Date.today, Date.today, Date.today)
+	end
+
+	private
+
+	def geocode_in_background
+		BackgroundGeocoder.perform_async(id)
 	end
 end
