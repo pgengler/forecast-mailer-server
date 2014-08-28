1a1cecd326750b5d5f81705c98d7ff11c528932c
Phil Gengler <phil@pgengler.net>
2014-08-28 13:06:56 -0400

    Use Postgres instead of SQLite for database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140826171614) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "subscriptions", force: true do |t|
     t.string   "location"
     t.date     "start"
