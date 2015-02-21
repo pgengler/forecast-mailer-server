90765cfe11e73f6a3c31be3af88ed89ea80345a5
Phil Gengler <phil@pgengler.net>
2015-02-21 10:37:55 -0500

    Only call geocoder on an existing subscription if the location was changed; this avoids an infinite loop ofgeocoding that was happening

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,8 @@ class Subscription < ActiveRecord::Base
 	validates :email, presence: true
 
 	geocoded_by :location
-	after_commit :geocode_in_background, on: [ :create, :update ]
+	after_commit :geocode_in_background, on: :create
+	after_validation :geocode_in_background, on: :update, if: ->(obj) { obj.location_changed? }
 
 	def self.active
 		where('("start" IS NULL AND "end" IS NULL) OR ("start" IS NULL AND ? <= "end") OR ("end" IS NULL AND "start" <= ?) OR (? BETWEEN "start" AND "end")', Date.today, Date.today, Date.today)
