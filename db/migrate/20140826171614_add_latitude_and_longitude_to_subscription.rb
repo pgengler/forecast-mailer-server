06545dd8b4eed41966d8cc6c087563fc4a33c019
Phil Gengler <phil@pgengler.net>
2017-06-15 17:32:17 -0400

    Update migration files

--- db/migrate/20140826171614_add_latitude_and_longitude_to_subscription.rb
+++ db/migrate/20140826171614_add_latitude_and_longitude_to_subscription.rb
@@ -1,4 +1,4 @@
-class AddLatitudeAndLongitudeToSubscription < ActiveRecord::Migration
+class AddLatitudeAndLongitudeToSubscription < ActiveRecord::Migration[4.2]
   def change
     add_column :subscriptions, :latitude, :float
     add_column :subscriptions, :longitude, :float
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,15 +15,15 @@ ActiveRecord::Schema.define(version: 20140826171614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "subscriptions", force: true do |t|
-    t.string   "location"
-    t.date     "start"
-    t.date     "end"
-    t.string   "email"
+  create_table "subscriptions", id: :serial, force: :cascade do |t|
+    t.string "location"
+    t.date "start"
+    t.date "end"
+    t.string "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "latitude"
-    t.float    "longitude"
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
