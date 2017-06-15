6adabecbfc98e4c49ac16e6b50af52dbceed2bc0
Phil Gengler <phil@pgengler.net>
2017-06-15 17:27:19 -0400

    Upgrade to Rails 5.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 4.2.0'
+gem 'rails', '~> 5.0.0'
 gem 'pg'
 gem 'sass-rails'
 gem 'uglifier', '>= 1.3.0'
@@ -29,3 +29,4 @@ group :development do
 end
 
 gem 'sinatra'
+gem 'rails-controller-testing'
--- test/controllers/subscriptions_controller_test.rb
+++ test/controllers/subscriptions_controller_test.rb
@@ -12,7 +12,7 @@ class SubscriptionsControllerTest < ActionController::TestCase
 	end
 
 	test "creates a new subscription with the given values" do
-		post :create, subscription: { location: 'Nowheresville', email: 'test@example.com', start: '2014-01-01', end: '2014-12-31' }
+		post :create, params: { subscription: { location: 'Nowheresville', email: 'test@example.com', start: '2014-01-01', end: '2014-12-31' } }
 		@subscription = assigns(:subscription)
 		assert_equal 'Nowheresville', @subscription.location
 		assert_equal 'test@example.com', @subscription.email
@@ -21,24 +21,24 @@ class SubscriptionsControllerTest < ActionController::TestCase
 	end
 
 	test "redirects to index after creating a new subscription" do
-		post :create, subscription: { location: 'Over there', email: 'example@example.com' }
+		post :create, params: { subscription: { location: 'Over there', email: 'example@example.com' } }
 		assert_response :redirect
 		assert_redirected_to subscriptions_path
 	end
 
 	test "displays a message after creating a new subscription" do
-		post :create, subscription: { location: 'Over there', email: 'dummy@example.com' }
+		post :create, params: { subscription: { location: 'Over there', email: 'dummy@example.com' } }
 		assert_equal 'Subscription created', flash[:success]
 	end
 
 	test "shows an 'edit' form" do
-		get :edit, id: subscriptions(:no_dates)
+		get :edit, params: { id: subscriptions(:no_dates) }
 		assert_response :success
 		assert_select 'form'
 	end
 
 	test "can update values for a subscription" do
-		put :update, id: subscriptions(:no_dates), subscription: { email: 'nobody@example.com', location: 'The Middle of Somewhere', start: '2014-01-01', end: '2014-02-01' }
+		put :update, params: { id: subscriptions(:no_dates), subscription: { email: 'nobody@example.com', location: 'The Middle of Somewhere', start: '2014-01-01', end: '2014-02-01' } }
 
 		subscription = assigns(:subscription)
 		assert_equal 'nobody@example.com', subscription.email
@@ -48,30 +48,30 @@ class SubscriptionsControllerTest < ActionController::TestCase
 	end
 
 	test "redirects to the index page after updating a subscription" do
-		put :update, id: subscriptions(:no_dates), subscription: { email: 'bill@example.com' }
+		put :update, params: { id: subscriptions(:no_dates), subscription: { email: 'bill@example.com' } }
 		assert_response :redirect
 		assert_redirected_to subscriptions_path
 	end
 
 	test "displays a message after updating a subscription" do
-		put :update, id: subscriptions(:no_dates), subscription: { start: '2014-01-01' }
+		put :update, params: { id: subscriptions(:no_dates), subscription: { start: '2014-01-01' } }
 		assert_equal 'Subscription updated', flash[:success]
 	end
 
 	test "can delete a subscription" do
 		assert_difference 'Subscription.count', -1 do
-			delete :destroy, id: subscriptions(:no_dates)
+			delete :destroy, params: { id: subscriptions(:no_dates) }
 		end
 	end
 
 	test "redirects back to the index page after destroying a subscription" do
-		delete :destroy, id: subscriptions(:no_dates)
+		delete :destroy, params: { id: subscriptions(:no_dates) }
 		assert_response :redirect
 		assert_redirected_to subscriptions_path
 	end
 
 	test "displays a message after destroying a subscription" do
-		delete :destroy, id: subscriptions(:no_dates)
+		delete :destroy, params: { id: subscriptions(:no_dates) }
 		assert_equal 'Subscription deleted', flash[:success]
 	end
 end
