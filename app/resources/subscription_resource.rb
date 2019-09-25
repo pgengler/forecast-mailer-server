6c63dbbf06712616eeb19411d6c5c58e828fa10b
Phil Gengler <phil@pgengler.net>
2019-09-25 19:38:49 -0400

    Send created_at/updated_at for subscriptions

--- app/resources/subscription_resource.rb
+++ app/resources/subscription_resource.rb
@@ -1,5 +1,5 @@
 class SubscriptionResource < JSONAPI::Resource
-  attributes :email, :end, :location, :start, :units
+  attributes :email, :end, :location, :start, :units, :created_at, :updated_at
 
   def end
     @model.end&.strftime('%Y-%m-%d')
@@ -7,5 +7,13 @@ class SubscriptionResource < JSONAPI::Resource
 
   def start
     @model.start&.strftime('%Y-%m-%d')
+  end
+
+  def self.updatable_fields(context)
+    super - [:created_at, :updated_at]
+  end
+
+  def self.creatable_fields(context)
+    self.updatable_fields context
   end
 end
