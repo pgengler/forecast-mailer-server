78c6c82d4302c70feb9dfe99ddc582621763adf7
Phil Gengler <phil@pgengler.net>
2020-10-19 18:26:28 -0400

    Use OpenWeatherMap data instead of Forecast.io

---
+++ app/lib/open_weather_map.rb
@@ -0,0 +1,68 @@
+require 'date'
+require 'json'
+require 'net/http'
+
+module OpenWeatherMap
+  class API
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    def forecast(latitude, longitude, units)
+      url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{latitude}&lon=#{longitude}&units=#{units}&exclude=current,minutely,hourly,alerts&appid=#{@api_key}"
+
+      response = Net::HTTP.get(URI.parse(url))
+
+      Forecast.new(response)
+    end
+  end
+
+  class Forecast
+    def initialize(api_response)
+      @response = JSON.parse(api_response)
+    end
+
+    def days
+      @response['daily'].map { |day| ForecastDay.new(day) }
+    end
+  end
+
+  class ForecastDay
+    def initialize(data)
+      @data = data
+    end
+
+    def date
+      DateTime.strptime(@data['dt'].to_s, '%s')
+    end
+
+    def icon_url
+      icon = @data['weather'].first['icon']
+      "http://openweathermap.org/img/wn/#{icon}@2x.png"
+    end
+
+    def summary
+      @data['weather'].first['description']
+    end
+
+    def low
+      @data['temp']['min']
+    end
+
+    def high
+      @data['temp']['max']
+    end
+
+    def precipitation_probability
+      @data['pop']
+    end
+
+    def sunrise
+      Time.at @data['sunrise']
+    end
+
+    def sunset
+      Time.at @data['sunset']
+    end
+  end
+end
--- app/workers/forecast_mailer_worker.rb
+++ app/workers/forecast_mailer_worker.rb
@@ -19,7 +19,8 @@ class ForecastMailerWorker
 	private
 
 	def get_forecast(subscription)
-		ForecastIO.api_key = Rails.application.secrets.forecastio_api_key
-		ForecastIO.forecast(subscription.latitude, subscription.longitude, params: { units: subscription.units })
+		api_key = Rails.application.secrets.openweathermap_api_key
+		units = subscription.units == 'en' ? 'imperial' : 'metric'
+		api.forecast(subscription.latitude, subscription.longitude, units)
 	end
 end
