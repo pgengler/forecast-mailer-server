b8ed9a50ab78d9ff304edca019b5cfc7215f7779
Phil Gengler <phil@pgengler.net>
2021-08-01 09:40:46 -0400

    Use Ruby 2.6.5 instead of 2.4.1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server 'hyperion.pgengler.net', user: 'forecast-mailer', roles: %w{web app db}
 set :rails_env, :production
 set :bundle_binstubs, nil
-set :rvm_ruby_version, '2.4.1@forecast-mailer'
+set :rvm_ruby_version, '2.6.5@forecast-mailer'
